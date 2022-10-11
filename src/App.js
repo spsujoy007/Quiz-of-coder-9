ef91b7b60ad7de356fda8bcb5e4ed28a5a423c28
Sujoy Paul <freelancersujoy@gmail.com>
2022-10-11 14:50:02 +0600

    error page created

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       ]}
     ])
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router}></RouterProvider>
     </div>  
   );
--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
     const error = useRouteError();
+    console.log(error);
     return (
-        <div>
-            <h1>404 error!</h1>
-            <p>This page can't found</p>
-        </div>
+        <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+		<div className="max-w-md text-center">
+			<h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+				<span className="sr-only">Error</span>404
+			</h2>
+			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+			<p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+			<Link to="/home">Back to home page</Link>
+		</div>
+	</div>
+</section>
     );
 };
 
