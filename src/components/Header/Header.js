c5f37fb2896c84bb04542c97a0ecca6d829bbd20
Sujoy Paul <freelancersujoy@gmail.com>
2022-10-12 17:50:53 +0600

    navbar responsive problem fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@ import './Header.css'
 const Header = () => {
   return (
     <div className=" bg-indigo-400">
-      <nav className="flex justify-between px-20 py-3">
+      <nav className="md:flex justify-between px-20 py-3">
         <div>
           <h1 className="text-4xl text-white uppercase">Quiz of Coder</h1>
         </div>
 
-        <div className="flex align-middle text-white font-bold">
+        <div className="navitems md:flex align-middle text-white font-bold mt-10 md:mt-0">
           <NavLink className={({ isActive }) => isActive ? 'active' : undefined}
            to="/home"><span className="navLinks">Home</span></NavLink>
           <NavLink aria-disabled className="" to="/topics"><span className="navLinks ">Topics</span></NavLink>
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Options from '../Options/Options';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
 
 const Topic = ({allquestion}) => {
     const {options, correctAnswer } = allquestion;
     const newQuestion = allquestion.question.split('<p>');
+    
+    const handleShowModal = () => {
+        toast('Correct answer is: ', `${correctAnswer}`)
+    }
 
     return (
         <div className='bg-indigo-100 my-5 p-5 rounded-2xl'>
             <div>
-               <div className='flex justify-around'>
+               <div className='flex justify-between align-middle'>
                  <h1 className='text-3xl text-indigo-500'>{newQuestion}</h1>
-                 <button className='p-10'>
-                 <EyeIcon className="h-6 w-6 text-blue-500"/>
+                 
+                 <button onClick={() => handleShowModal()} className='p-10'>
+                    <EyeIcon className="h-6 w-6 text-blue-500"/>
                  </button>
+                 {/* <ToastContainer/> */}
                </div>
                 <div className='px-10 py-0 my-5'>
                     <ul className='list-decimal'>
