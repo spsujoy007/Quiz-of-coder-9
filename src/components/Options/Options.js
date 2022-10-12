b4900ededb171093b732c98d4d713deb817f624d
Sujoy Paul <freelancersujoy@gmail.com>
2022-10-12 15:31:08 +0600

    eye button added for see the correct answer

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Options = ({option, allquestion, correctAnswer }) => {
-  
-// const correctanswer = 
+const Options = ({ option, allquestion, correctAnswer }) => {
+  // const correctanswer =
 
-    const handleShowResult = () => {
-        // console.log('Answer: ', allquestion.correctAnswer);
-        if(option === correctAnswer){
-            toast("Answers is correct!")
-        }
-        else{
-            toast("Wrong answer!")
-        }
+  const handleShowResult = () => {
+    // console.log('Answer: ', allquestion.correctAnswer);
+    if (option === correctAnswer) {
+      toast("Answers is correct!");
+    } else {
+      toast("Wrong answer!");
     }
-    
-    return (
+  };
 
-           <button onClick={() => handleShowResult() } className='w-full'>
-             <li className='hover:bg-indigo-300 my-1 text-2xl text-indigo-500 p-3 rounded-xl bg-white text-center'>{option}</li>
-             <ToastContainer
-position="top-right"
-autoClose={1000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/>
-           </button>
-     );
+  return (
+    <button onClick={() => handleShowResult()} className="w-full">
+      <li className="hover:bg-indigo-300 my-1 text-2xl text-indigo-500 p-3 rounded-xl bg-white text-center">
+        {option}
+      </li>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </button>
+  );
 };
 
 export default Options;
--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import Options from '../Options/Options';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Topic = ({allquestion}) => {
     const {options, correctAnswer } = allquestion;
     const newQuestion = allquestion.question.split('<p>');
 
     return (
-        <div className='bg-indigo-100 my-2 p-5 rounded-2xl'>
-            <div className='text-center'>
-                <h1 className='text-3xl text-indigo-500'>{newQuestion}</h1>
-                <div className='p-10 my-5'>
+        <div className='bg-indigo-100 my-5 p-5 rounded-2xl'>
+            <div>
+               <div className='flex justify-around'>
+                 <h1 className='text-3xl text-indigo-500'>{newQuestion}</h1>
+                 <button className='p-10'>
+                 <EyeIcon className="h-6 w-6 text-blue-500"/>
+                 </button>
+               </div>
+                <div className='px-10 py-0 my-5'>
                     <ul className='list-decimal'>
  
                         {
                             options.map(option => <Options key={option.id} option={option} allquestion={allquestion} correctAnswer={correctAnswer}></Options> )
                         }
+                        
                     </ul>
                 </div>
             </div>
